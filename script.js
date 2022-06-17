201137ae1f950394b29f681690a1e8fbf80d1cca
Jesse <dodderjesse@gmail.com>
2022-06-17 14:11:15 -0400

    Add variable functions and set up buttons

--- script.js
+++ script.js
@@ -15,6 +15,42 @@ const zero = document.getElementById('zeroBtn')
 const dec = document.getElementById('decimalBtn')
 const clear = document.getElementById('clearBtn')
 
+let num1 = null
+let num2 = null
+let operator = null
+
+plus.onclick = () => setOperator('add')
+minus.onclick = () => setOperator('subtract')
+division.onclick = () => setOperator('divide')
+multi.onclick = () => setOperator('multiply')
+nine.onclick = () => setNum('9')
+eight.onclick = () => setNum('8')
+seven.onclick = () => setNum('7')
+six.onclick = () => setNum('6')
+five.onclick = () => setNum('5')
+four.onclick = () => setNum('4')
+three.onclick = () => setNum('3')
+two.onclick = () => setNum('2')
+one.onclick = () => setNum('1')
+zero.onclick = () => setNum('0')
+
+function setNum (num) {
+    if ((num1 === null) && (operator=== null)) {
+        num1 = num
+    } else if (num1 && operator === null) {
+        num1 += num
+    }
+    if (operator != null && !num2) {
+        num2 = num
+    } else if ((operator != null) && num2) {
+        num2 += num
+    }
+}
+
+function setOperator(opp) {
+    operator = opp;
+}
+
 function add(a, b) {
     a = Number(a);
     b = Number(b);
