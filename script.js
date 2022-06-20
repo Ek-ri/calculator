dc416249d4a04bc052c3518014461149d6831526
Jesse <dodderjesse@gmail.com>
2022-06-20 11:42:23 -0400

    Add display reactivity

--- script.js
+++ script.js
@@ -15,10 +15,12 @@ const zero = document.getElementById('zeroBtn')
 const dec = document.getElementById('decimalBtn')
 const clear = document.getElementById('clearBtn')
 const equal = document.getElementById('equal')
+const display = document.getElementById('display')
 
 let num1 = null
 let num2 = null
 let operator = null
+let symbol = null
 let result = null
 
 plus.onclick = () => setOperator('add')
@@ -40,18 +42,35 @@ equal.onclick = () => operate(num1, num2, operator)
 function setNum (num) {
     if ((num1 === null) && (operator=== null)) {
         num1 = num
+        display.textContent = num1
     } else if (num1 && operator === null) {
         num1 += num
+        display.textContent = num1
     }
     if (operator != null && !num2) {
         num2 = num
+        display.textContent = `${num1} ${symbol} ${num2}`
     } else if ((operator != null) && num2) {
         num2 += num
+        display.textContent = `${num1} ${symbol} ${num2}`
     }
 }
 
 function setOperator(opp) {
     operator = opp;
+    if (opp === 'add') {
+        symbol = '+'
+        display.textContent = `${num1} ${symbol}`
+    } else if (opp === 'subtract') {
+        symbol = '-'
+        display.textContent = `${num1} ${symbol}` 
+    } else if (opp === 'multiply') {
+        symbol = '*'
+        display.textContent = `${num1} ${symbol}`
+    } else {
+        symbol = '/'
+        display.textContent = `${num1} ${symbol}`
+    }
 }
 
 function add(a, b) {
@@ -93,4 +112,5 @@ function operate(num1, num2, operator) {
     if (operator === "divide") {
         result = divide(num1, num2);
     }
+    display.textContent = result
 }
